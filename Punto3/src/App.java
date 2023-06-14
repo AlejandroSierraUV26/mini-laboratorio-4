e2cf11ee5cc1a72fe98c262a6bab7a275b820f3f
AlejandroSierraUV26 <104778436+AlejandroSierraUV26@users.noreply.github.com>
2023-06-14 09:20:24 -0500

    Punto 3 terminado

--- Punto3/src/App.java
+++ Punto3/src/App.java
@@ -1,10 +1,34 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        String palabra = scanner.nextLine();
-        System.out.println(palabra);
-        scanner.close();
+public class App{
+    public static void encontrarOcurrencias(String palabra, char letra) {
+        LinkedList<Integer> ocurrencias = new LinkedList<>();
+
+        for (int i = 0; i < palabra.length(); i++) {
+            if (palabra.charAt(i) == letra) {
+            ocurrencias.add(i);
+            }
+        }
+
+        if (!ocurrencias.isEmpty()) {
+            int primero = ocurrencias.getFirst();
+            int ultimo = ocurrencias.getLast();
+            System.out.println("Primera ocurrencia de " + letra + ": " + primero);
+            System.out.println("Última ocurrencia de " + letra + ": " + ultimo);
+        } 
+        else {
+            System.out.println("La letra " + letra + " no se encuentra en la palabra.");
+        }
     }
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Ingrese una palabra: ");
+    String palabra = scanner.nextLine();
+    System.out.print("Ingrese la letra a buscar: ");
+    char letra = scanner.next().charAt(0);
+    encontrarOcurrencias(palabra, letra);
+    scanner.close();
+   }
 }
